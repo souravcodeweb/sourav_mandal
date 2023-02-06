1090f838ad4518e463fea5906490849a2d1091df
sourav <souravmandal909p@gmail.com>
2023-02-06 04:55:25 +0000

    add all chenges

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Ui/Navbar';
+import Home from "./Components/Pages/Home";
+import About from "./Components/Pages/About";
+import Work from "./Components/Pages/Work";
+import Videos from "./Components/Pages/Videos";
+import Blogs  from "./Components/Pages/Blogs";
+import Contact from "./Components/Pages/Contact";
 
 function App() {
   return (
-    <div className='mine'>
-      <h1>Sourav Mandal</h1>
-    </div>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='about' element={<About/>}/>
+        <Route path='work' element={<Work/>}/>
+        <Route path='videos' element={<Videos/>}/>
+        <Route path='blogs' element={<Blogs/>}/>
+        <Route path='contact' element={<Contact/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
