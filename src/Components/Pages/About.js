de1f302f4ed00bfcb166608a6edc22f6f2a138ee
sourav <souravmandal909p@gmail.com>
2023-02-10 05:37:38 +0000

    add all the home page new code

--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import "./About.css";
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <h1>About</h1>
+    </div>
   )
 }
 
